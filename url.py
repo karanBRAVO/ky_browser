84c5d14a92b22316bd6f26d0abf97e4ec7d2b681
D@reK@r@n18 <xpresskaran98@gmail.com>
2025-05-31 11:37:44 +0530

    add functionality to download pages
    Signed-off-by: D@reK@r@n18 <xpresskaran98@gmail.com>

---
+++ url.py
@@ -0,0 +1,187 @@
+import socket
+import ssl
+import os
+import gzip
+from datetime import datetime, timedelta, timezone
+
+
+class URL:
+    SUPPORTED_SCHEMES = ("http", "https", "file", "data", "view-source", "about")
+
+    def __init__(self, url: str):
+        self.s = None  # socket
+        self.cache = {}
+
+        try:
+            if url.startswith("http"):
+                self.scheme, url = url.split("://", 1)
+            else:
+                self.scheme, url = url.split(":", 1)
+
+            assert self.scheme in self.SUPPORTED_SCHEMES, "Invalid URL scheme"
+
+            if self.scheme == "http" or self.scheme == "https":
+                if self.scheme == "http":
+                    self.port = 80
+                elif self.scheme == "https":
+                    self.port = 443
+
+                if "/" not in url:
+                    url += "/"
+                self.host, url = url.split("/", 1)
+                if ":" in self.host:
+                    self.host, port = self.host.split(":", 1)
+                    self.port = int(port)
+                self.path = "/" + url
+            elif self.scheme == "file":
+                url = url.split("/", 1)[-1]
+                assert os.path.exists(url), "File does not exist"
+                self.path = url
+            elif self.scheme == "data":
+                self.mediaType, self.content = url.split(",", 1)
+            elif self.scheme == "view-source":
+                self.url = url
+            elif self.scheme == "about":
+                self.path = url
+        except Exception as e:
+            print(f"Error parsing URL: {e}")
+
+    def request(self):
+        try:
+            if self.scheme == "http" or self.scheme == "https":
+                url = f"{self.scheme}://{self.host}:{self.port}{self.path}"
+                if url in self.cache:
+                    cached = self.cache[url]
+                    cacheMaxAge = int(cached.get("max-age", 0))
+                    cacheDate = datetime.strptime(
+                        cached.get("date", ""), "%a, %d %b %Y %H:%M:%S GMT"
+                    )
+                    cacheDate = cacheDate.replace(tzinfo=timezone.utc)
+
+                    expirationTime = cacheDate + timedelta(seconds=cacheMaxAge)
+                    now = datetime.now(timezone.utc)
+
+                    if expirationTime > now:
+                        return cached["content"]
+
+                if self.s is None:
+                    self.s = socket.socket(
+                        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP
+                    )
+                    self.s.connect((self.host, self.port))
+
+                    ctx = ssl.create_default_context()
+                    if self.scheme == "https":
+                        self.s = ctx.wrap_socket(self.s, server_hostname=self.host)
+
+                requestHeaders = {
+                    "Host": self.host,
+                    "Connection": "keep-alive",  # close or keep-alive
+                    "User-Agent": "Ky_Browser",
+                    "Accept-Encoding": "gzip",
+                }
+                request = f"GET {self.path} HTTP/1.1\r\n"
+                for name, value in requestHeaders.items():
+                    request += f"{name}: {value}\r\n"
+                request += "\r\n"  # End of headers
+
+                self.s.send(request.encode("utf-8"))
+
+                response = self.s.makefile("rb", encoding="utf-8", newline="\r\n")
+                statusLine = response.readline().decode("utf-8").strip()
+                version, status, reason = statusLine.split(" ", 2)  # HTTP/1.0 200 OK
+                status = int(status)
+
+                responseHeaders = {}
+                while True:
+                    line = response.readline().decode("utf-8")
+                    if line == "\r\n":
+                        break
+                    name, value = line.split(":", 1)
+                    responseHeaders[name.strip().casefold()] = value.strip()
+
+                contentLength = int(responseHeaders.get("content-length", 0))
+                content = response.read(contentLength)
+
+                if responseHeaders.get("content-encoding", "") == "gzip":
+                    content = gzip.decompress(content).decode("utf-8")
+                else:
+                    content = content.decode("utf-8")
+
+                # redirects
+                if status >= 300 and status < 400:
+                    location = responseHeaders.get("location")
+                    if location:
+                        if not location.startswith("http://"):
+                            if not location.startswith("/"):
+                                location = "/" + location
+                            location = (
+                                f"{self.scheme}://{self.host}:{self.port}{location}"
+                            )
+                        return URL(location).request()
+
+                if requestHeaders.get("Connection", "").lower() == "close":
+                    self.s = self.s.close()
+
+                cacheControl = responseHeaders.get("cache-control", "")
+
+                if cacheControl and cacheControl.find("max-age=") != -1:
+                    maxAge = cacheControl.split("max-age=")[-1]
+                    if maxAge.find(",") != -1:
+                        maxAge = int(maxAge.split(",")[0])
+                    self.cache[url] = {
+                        "content": content,
+                        "etag": responseHeaders.get("etag", ""),
+                        "content-type": responseHeaders.get("content-type", ""),
+                        "cache-control": cacheControl,
+                        "date": responseHeaders.get("date", ""),
+                        "max-age": maxAge,
+                    }
+
+                return content
+            elif self.scheme == "file":
+                try:
+                    with open(self.path, "r", encoding="utf-8") as file:
+                        return file.read()
+                except FileNotFoundError:
+                    return ""
+            elif self.scheme == "data":
+                return self.content
+            elif self.scheme == "view-source":
+                view_source_url = URL(self.url)
+                return view_source_url.request()
+            elif self.scheme == "about":
+                if self.path == "blank":
+                    return "<html><body></body></html>"
+                return "<html><body><h1>About Page</h1></body></html>"
+        except Exception as e:
+                print(f"Error: {e}")
+                return URL("about:blank").request()
+
+
+def show(body):
+    in_tag = False
+    for char in body:
+        if char == "<":
+            in_tag = True
+        elif char == ">":
+            in_tag = False
+        elif not in_tag:
+            print(char, end="")
+
+
+def load(url: URL):
+    content = url.request()
+    show(content)
+
+
+if __name__ == "__main__":
+    url = URL("https://example.org/index.html")
+    # url = URL("https:/example.org/index.html") # invalid URL for testing
+    # url = URL("https://browser.engineering/im/http-tls-2.gif")
+    # url = URL("http://localhost:8080")
+    # url = URL("file:///E:/ky_browser/main.py")
+    # url = URL("data:text/html,<h1>Hello World!</h1>")
+    # url = URL("view-source:http://example.org/")
+    # url = URL("http://browser.engineering/redirect3")
+    load(url)
